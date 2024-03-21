d83d48656323ab71fa67bb95e5d63a180ac6ae5b
juanpablo <juanpablo.zuritasoto@gmail.com>
2024-03-21 13:02:43 +0100

    Modificado

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ const fs = require('fs');
 const app = express();
 const ajv = new Ajv();
 
-// Cargar esquemas de validación desde el directorio "schemas"
+
 const schema1 = JSON.parse(fs.readFileSync('schemas/schema1.json', 'utf8'));
 const schema2 = JSON.parse(fs.readFileSync('schemas/schema2.json', 'utf8'));
 
-// Compilar esquemas con Ajv
+
 const validateSchema1 = ajv.compile(schema1);
 const validateSchema2 = ajv.compile(schema2);
 
-// Middleware para manejar solicitudes POST
+
 app.use(express.json());
 
-// Ruta para validar JSON contra schema1.json
+
 app.post('/ruta1', (req, res) => {
     const isValid = validateSchema1(req.body);
     if (isValid) {
@@ -26,7 +26,7 @@ app.post('/ruta1', (req, res) => {
     }
 });
 
-// Ruta para validar JSON contra schema2.json
+
 app.post('/ruta2', (req, res) => {
     const isValid = validateSchema2(req.body);
     if (isValid) {
@@ -36,7 +36,7 @@ app.post('/ruta2', (req, res) => {
     }
 });
 
-// Iniciar servidor
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor iniciado en el puerto ${PORT}`);
